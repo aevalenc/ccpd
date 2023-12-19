7fabbcdadc2279a2a3ad7c1f7cb2e2f4b873cfc8
aevalenc <aevalenc@syr.edu>
2023-12-18 17:35:04 -0800

    Implement attr classes for new input class

--- data_types/inputs.py
+++ data_types/inputs.py
@@ -9,7 +9,7 @@
              mat   : Compressor material
 """
 
-from attrs import frozen
+from attrs import frozen, field
 
 
 class DesignParameters:
@@ -32,7 +32,7 @@ class Inputs:
     """
 
     def __init__(self, input_dictionary) -> None:
-        # self.__dict__.update(input_dictionary)
+        self.__dict__.update(input_dictionary)
         self.mass_flow_rate = input_dictionary.get("mass_flow_rate")
         self.inlet_total_pressure = input_dictionary.get("inlet_total_pressure")
         self.inlet_total_temperature = input_dictionary.get("inlet_total_temperature")
@@ -58,17 +58,31 @@ class InputsII:
 
 
 @frozen
-class DesignInputs:
-    mass_flow_rate: float
-    inlet_total_pressure: float
-    inlet_total_temperature: float
-    compression_ratio: float
-    surface_roughness: float
-    tip_clearance: float
+class DesignParametersII:
+    """
+    Design Parameters version II
+    """
+
     hub_diameter: float
-    outlet_angle_guess: float
     specific_diameter: float
     specific_rotational_speed: float
     end_to_end_efficiency: float
-    fluid: str
-    material: str
+    fluid: float
+    material: float
+
+
+@frozen
+class DesignInputs:
+    mass_flow_rate = field(converter=float)  # type: ignore
+    inlet_total_pressure = field(converter=float)  # type: ignore
+    inlet_total_temperature = field(converter=float)  # type: ignore
+    compression_ratio = field(converter=float)  # type: ignore
+    surface_roughness = field(converter=float)  # type: ignore
+    tip_clearance = field(converter=float)  # type: ignore
+    hub_diameter = field(converter=float)  # type: ignore
+    outlet_angle_guess = field(converter=float)  # type: ignore
+    specific_diameter = field(converter=float)  # type: ignore
+    specific_rotational_speed = field(converter=float)  # type: ignore
+    end_to_end_efficiency = field(converter=float)  # type: ignore
+    fluid = field(type=str)  # type: ignore
+    material = field(type=str)  # type: ignore
